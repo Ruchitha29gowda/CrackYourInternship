29ec358a346f82f10b0a11f7550900afcb335411
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-07-16 01:11:30 +0530

    Find all Duplicate Number

---
+++ FinadAllDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class FinadAllDuplicate {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1]){
+                list.add(nums[i]);
+            }
+        }
+
+        return list;
+    }
+}
