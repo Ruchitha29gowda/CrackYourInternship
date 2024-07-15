67d17ff4a568ec0acf64cf185739ecbad41a373d
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-07-15 22:23:01 +0530

    remove duplicates and Move zeros

---
+++ MoveZeros.java
@@ -0,0 +1,13 @@
+class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int indx=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                nums[indx++]=nums[i];
+            }
+        }
+        for(int i=indx; i<nums.length;i++){
+            nums[indx++]=0;
+        }
+    }
+}
