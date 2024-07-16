9c4549d5d5372d151bf9bbd449b12c5940b305df
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-07-16 22:54:28 +0530

    Chocolate Distribution problem

---
+++ ChocolateDistributionProblem.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ChocolateDistributionProblem {
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        int ans = Integer.MAX_VALUE;
+        for(int i=0; (i+m-1)<n; i++){
+            ans = Math.min(ans, a.get(i+m-1) - a.get(i));
+        }
+        return ans;
+    }
+}
+
