375990aee36a32c0402a433b35912b0646e61116
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-07-21 01:02:44 +0530

    PrintAnagramsTogether

--- string/PrintAnagramsTogether.java
+++ string/PrintAnagramsTogether.java
@@ -5,25 +5,22 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class GroupAnagrams {
-    
-}
-
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+public class PrintAnagramsTogether {
+     public List<List<String>> Anagrams(String[] string_list) {
+        // Code here
         HashMap<String , List<String>> map = new HashMap<>();
 
-        for(int i=0; i<strs.length; i++){
-            char[] ch = strs[i].toCharArray();
+        for(int i=0; i<string_list.length; i++){
+            char[] ch = string_list[i].toCharArray();
             Arrays.sort(ch);
             String newStr = new String(ch);
 
             if(!map.containsKey(newStr)){
                 map.put(newStr, new ArrayList<>());
             }
-            map.get(newStr).add(strs[i]);
+            map.get(newStr).add(string_list[i]);
         }
-
         return new ArrayList<>(map.values());
     }
 }
+
